4ba370a217d71d87634ecc1cd01b558bc871cba5
Zachary Drayer <zacharydrayer@gmail.com>
2022-03-23 18:52:06 -0700

    update more of Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,26 @@
 import PackageDescription
 
 let package = Package(
-	name: "OG"
+    name: "OG",
+    platforms: [
+		.macOS(.v10_10), .iOS(.v10), .tvOS(.v9), .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "OG", targets: ["OG"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "OG",
+            dependencies: [],
+            path: "Sources",
+            exclude: ["Info.plist"]
+        ),
+         .testTarget(
+            name: "Tests",
+            dependencies: ["OG"],
+            path: "Tests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
